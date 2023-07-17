b51fefbb1255ad223a08c3a4d53ac8681b2fcc4b
Nobue.S <nobutako.info@gmail.com>
2023-07-17 10:12:10 -0400

    Apply css and add a logo image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css';
+import { Outlet, Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
 
 function NavBar() {
   return (
-    <nav className="bg-green-200 p-6">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="w-full overflow-auto">
+        <ul className="flex">
+          <li className="w-1/4 flex justify-center items-center">
+            <Link to="/about" className="float-left text-white">ABOUT</Link>
+         </li>
+         <li className="w-1/4 flex justify-center items-center">
+            <Link to="/skills" className="float-left text-white ">SKILLS</Link>
+          </li>
+          <li className="w-1/4 flex justify-center items-center">
+            <Link to="/" className="float-left"><img src={logo} alt="Logo" className="h-10 w-auto" /></Link>
+          </li>
+          <li className="w-1/4 flex justify-center items-center">
+            <Link to="/projects" className="float-left text-white">PROJECTS</Link>
+          </li>
+          <li className="w-1/4 flex justify-center items-center">
+            <Link to="/contact" className="float-left text-white">CONTACT</Link>
+          </li>
+       </ul>
+      </nav>
+
+      <Outlet />
+    </>
   );
 }
 
