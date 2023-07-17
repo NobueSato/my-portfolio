5c952d9f65468eac749cfe2b936e1436c0924a74
Nobue.S <nobutako.info@gmail.com>
2023-07-17 09:54:53 -0400

    Rename AboutPage component to About

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,26 @@
-import React from "react";
+import image from "../assets/images/about.png";
 
 function About() {
   return (
-    <div>
-      <h2>Education</h2>
-      <p>Diploma in computer programming at Seneca college applied arts and technology</p>
-      <h2>Work</h2>
-      <p>Associate Software Engineer at Deloitte starts from May 2022</p>
-    </div>
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+            {/* <!-- Image column --> */}
+            <div class="md:order-first flex justify-center">
+              <img src={image} alt="About title" class="object-contain" />
+            </div>
+
+            {/* <!-- Paragraph column --> */}
+            <div class="md:order-last px-10">
+              <p class="text-xl font-semibold mb-4">Hi I’m Nobue Sato</p>
+              <p class="text-gray-700">
+                With one year of experience as a software engineer under my belt, 
+                I am known for my dedication, 
+                collaborative work style, 
+                and strong commitment to continuous learning through projects and initiatives. 
+                I strive to create solutions that improve people's daily lives 
+                and make a difference in the world.
+              </p>
+            </div>
+          </div>
   );
 }
 
