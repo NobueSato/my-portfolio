dc39d48ecc1dd9b191cb9be249deaca0e2457fde
Nobue.S <nobutako.info@gmail.com>
2023-08-04 12:44:59 -0400

    Recreate NavBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import { Outlet, Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
+
+function NavBar() {
+  return (
+    <>
+      <nav className="nav-bar w-full overflow-hidden">
+        <ul className="flex">
+          <li className="w-1/4 flex justify-center items-center">
+            <Link to="/about">ABOUT</Link>
+         </li>
+         <li>
+            <Link to="/skills">SKILLS</Link>
+          </li>
+          <li>
+            <Link to="/"><img src={logo} alt="Logo" className="h-32 w-auto" /></Link>
+          </li>
+          <li>
+            <Link to="/projects">PROJECTS</Link>
+          </li>
+          <li>
+            <Link to="/contact">CONTACT</Link>
+          </li>
+       </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  );
+}
+
+export default NavBar;
