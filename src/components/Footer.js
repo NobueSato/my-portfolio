7e87326e3e83a2b5892e7c8a8d70340d4930474b
Nobue.S <nobutako.info@gmail.com>
2023-08-16 13:11:09 -0400

    Add p element to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
       <div className="container mx-auto text-center">
         <p>&copy; 2023 Nobue Sato. All rights reserved.</p>
         <p>Designed and developed by Nobue Sato</p>
+        <p>Free images are from Canva</p>
       </div>
     </footer>
   );
