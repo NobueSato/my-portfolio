9bf8238736708284e4f0b6964fde9e367bfff082
Nobue.S <nobutako.info@gmail.com>
2023-08-24 15:32:49 -0400

    Add reference links for the icon iamges

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,8 @@ const Footer = () => {
         <p>&copy; 2023 Nobue Sato. All rights reserved.</p>
         <p>Designed and developed by Nobue Sato</p>
         <p>Free images are from Canva</p>
+        <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/447/linkedin">LinkedIn</a> icon by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
+        <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/12598/github">GitHub</a> icon by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
       </div>
     </footer>
   );
