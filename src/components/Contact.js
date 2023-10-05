ed5465a48ed6b98dd20d42218fd87d30c0fcfccf
Nobue.S <nobutako.info@gmail.com>
2023-10-04 22:05:38 -0400

    Add logic to use emailJS

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,35 @@
 import headerImg from "../assets/images/contact.png";
+import emailjs from "emailjs-com"; // For emailjs-com package
+import { useEffect } from "react";
 import { Card } from "./Skills";
 
 const Contact = () => {
+  useEffect(() => {
+    emailjs.init(process.env.REACT_APP_EMAILJS_PUBLIC_KEY);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target; // Get the form element
+
+    // Generate a random contact number
+    form.contact_number.value = (Math.random() * 100000) | 0;
+
+    // Send the form data via EmailJS
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        form
+      )
+      .then((response) => {
+        console.log("SUCCESS!", response);
+      })
+      .catch((error) => {
+        console.error("FAILED...", error);
+      });
+  };
+
   return (
     <>
       <div className="section-header flex justify-center items-center h-30">
@@ -13,15 +41,48 @@ const Contact = () => {
       </div>
       <div className="contact-form flex justify-center items-center h-5/6 ml-42">
         <Card title="Contact me">
-          <form name="contact" method="POST" data-netlify="true" className="flex flex-col gap-4">
-            <input type="hidden" name="form-name" value="contact" className="hidden" />
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" placeholder="Name" className="border p-2"/>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Email" className="border p-2"/>
-            <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" placeholder="Message" className="border p-2" rows="4"></textarea>
-            <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+          <form
+            id="contact-form"
+            onSubmit={handleSubmit}
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            className="flex flex-col gap-4"
+          >
+            <input
+              type="hidden"
+              name="contact_number"
+              value="contact"
+              className="hidden"
+            />
+            <label htmlFor="user_name">Name</label>
+            <input
+              type="text"
+              name="from_name"
+              id="user_name"
+              placeholder="Name"
+              className="border p-2"
+            />
+            <label htmlFor="user_email">Email</label>
+            <input
+              type="email"
+              name="reply_to"
+              id="user_email"
+              placeholder="Email"
+              className="border p-2"
+            />
+            <label htmlFor="user_message">Message</label>
+            <textarea
+              name="message"
+              id="user_message"
+              placeholder="Message"
+              className="border p-2"
+              rows="4"
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+            >
               Submit
             </button>
           </form>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import App from './App';
-import './styles/global.scss'; // Import the global CSS file
+import React from "react";
+import ReactDOM from "react-dom/client";
+import emailjs from "emailjs-com"; // For emailjs-com package
+import App from "./App";
+import "./styles/global.scss"; // Import the global CSS file
 
 // the traditional and actual entry point for all node apps
 // in React it hass code of what to render and where to render.
@@ -9,7 +11,19 @@ import './styles/global.scss'; // Import the global CSS file
 // import ReactDOM from 'react-dom';
 // ReactDOM.render(<App />, document.getElementById('root'));
 
+(function () {
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = `https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js`;
+
+  script.onload = function () {
+    // Initialize EmailJS with the correct public key from your dashboard
+    emailjs.init(process.env.REACT_APP_EMAILJS_PUBLIC_KEY);
+  };
+
+  document.head.appendChild(script); // Append to the <head> section
+})();
 // After (React18)
-import ReactDOM from 'react-dom/client';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
