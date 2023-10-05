d8cffa5080ea5d2d85a87225d43838d5ff10922c
Nobue.S <nobutako.info@gmail.com>
2023-10-04 22:03:07 -0400

    Change styles for navBar and Skills components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,24 +4,27 @@ import logo from "../assets/images/logo.png";
 function NavBar() {
   return (
     <>
-      <nav className="nav-bar w-full overflow-hidden">
-        <ul className="flex">
-          <li className="w-1/4 flex justify-center items-center">
+      <nav className="nav-bar">
+        <ul className="flex justify-center">
+          {/* Make them react-scroll link later */}
+          {/* <li className="w-1/4 flex justify-center items-center">
             <Link to="/about">ABOUT</Link>
          </li>
          <li>
             <Link to="/skills">SKILLS</Link>
-          </li>
+          </li> */}
           <li>
-            <Link to="/"><img src={logo} alt="Logo" className="h-32 w-auto" /></Link>
+            <Link to="/">
+              <img src={logo} alt="Logo" />
+            </Link>
           </li>
-          <li>
+          {/* <li>
             <Link to="/projects">PROJECTS</Link>
           </li>
           <li>
             <Link to="/contact">CONTACT</Link>
-          </li>
-       </ul>
+          </li> */}
+        </ul>
       </nav>
 
       <Outlet />
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,5 @@
 import titleimg from "../assets/images/skills.png";
-import data from '../assets/data.json';
+import data from "../assets/data.json";
 
 export function Card({ title, colour = "purple", children }) {
   const windowClass = `window-${colour}-bg`;
@@ -9,15 +9,15 @@ export function Card({ title, colour = "purple", children }) {
     <div className={windowClass}>
       {/* Title Bar */}
       <div className={titleBarClass}>
-       {/* Pink Button */}
-      <div className="pink-button"></div>
-      {/* Yellow Button */}
-      <div className="yellow-button"></div>
-      {/* White Button */}
-      <div className="white-button"></div>
-      <span className="text-white 2xl:text-3xl absolute left-1/2 transform -translate-x-1/2">{title ? title : ''}</span>
-      </div> {/* The end of title bar */}
-
+        {/* Pink Button */}
+        <div className="pink-button"></div>
+        {/* Yellow Button */}
+        <div className="yellow-button"></div>
+        {/* White Button */}
+        <div className="white-button"></div>
+        <span className="card-title">{title ? title : ""}</span>
+      </div>{" "}
+      {/* The end of title bar */}
       {/* Content for the card */}
       <div className="card-content bg-white rounded-b-lg shadow-md flex flex-col items-center justify-center h-full">
         {children}
@@ -34,15 +34,19 @@ function Skills() {
     <>
       <div className="section-header">
         <img src={titleimg} alt="Skills title" className="h-full w-auto" />
-        <h2 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0">Skills</h2>
+        <h2 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0">
+          Skills
+        </h2>
       </div>
 
       <div className="grid-wrapper grid grid-cols-3 gap-4">
         {/* Generate Card by mapping */}
-        {skillsTitles.map(title => (
+        {skillsTitles.map((title) => (
           <Card key={title} title={title} colour="blue">
-            {skills[title].map(skill => (
-              <p key={skill}>{skill}</p>
+            {skills[title].map((skill) => (
+              <p key={skill} className="text-4xl">
+                {skill}
+              </p>
             ))}
           </Card>
         ))}
